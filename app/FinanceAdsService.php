a3d50a429e899099deec3b1bee2dd9927dae225e
Mohammad Reza Golestan <mreza_golestan@live.com>
2025-02-25 12:54:53 +0330

    figure a way out to resolve broken read for link (CDATA in XML)

--- app/FinanceAdsService.php
+++ app/FinanceAdsService.php
@@ -11,9 +11,10 @@ class FinanceAdsService
 
     public function getCreditCardComparison(): ?array
     {
+
         $data = Cache::rememberForever('test', function(){
             $xmlContent = file_get_contents('https://tools.financeads.net/webservice.php?wf=1&format=xml&calc=kreditkarterechner&country=ES');
-            $xmlObject = simplexml_load_string($xmlContent);
+            $xmlObject = simplexml_load_string($xmlContent, 'SimpleXMLElement', LIBXML_NOCDATA);
             $array = json_decode(json_encode($xmlObject));
             return $array?->product;
         });
